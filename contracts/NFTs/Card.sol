dc058bbda99df2045f100f6262847874f9ec6468
luthebao <luthebao97@gmail.com>
2024-01-31 02:46:42 +0700

    update URI

--- contracts/NFTs/Card.sol
+++ contracts/NFTs/Card.sol
@@ -45,6 +45,8 @@ contract CARDNFT is ERC721, ERC721Enumerable, ERC721Burnable, AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, address(msg.sender));
         _grantRole(MINTER_ROLE, address(msg.sender));
         storageNFT = IStorage(address(_store));
+        useURI = true;
+        _uri = "https://bbb-api.vercel.app/api/nft/";
     }
 
     function toggleUseURI() external onlyRole(DEFAULT_ADMIN_ROLE) {
